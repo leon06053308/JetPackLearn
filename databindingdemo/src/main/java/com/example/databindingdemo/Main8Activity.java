017870bf28b49cfbe860e9835a17a6794f3878ef
youqiangcao <youqiangcao@ecarx.com.cn>
2018-11-19 16:48:01 +0800

    xxx
    Signed-off-by: youqiangcao <youqiangcao@ecarx.com.cn>

--- databindingdemo/src/main/java/com/example/databindingdemo/Main8Activity.java
+++ databindingdemo/src/main/java/com/example/databindingdemo/Main8Activity.java
@@ -2,12 +2,15 @@ package com.example.databindingdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.databinding.DataBindingUtil;
+import com.example.databindingdemo.databinding.ActivityMain8Binding;
 
 public class Main8Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main8);
+        //setContentView(R.layout.activity_main8);
+        ActivityMain8Binding activityMain8Binding = DataBindingUtil.setContentView(this, R.layout.activity_main8);
     }
 }
